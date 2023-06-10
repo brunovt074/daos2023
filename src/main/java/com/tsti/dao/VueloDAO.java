d5705698e0e0b478df89c511a15f483e68a6b0ee
brunovt074 <brunovt.074@gmail.com>
2023-06-10 20:02:25 -0300

    agregadas interfaces DAO restantes

--- src/main/java/com/tsti/dao/VueloDAO.java
+++ src/main/java/com/tsti/dao/VueloDAO.java
@@ -1,10 +1,12 @@
 package com.tsti.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.tsti.entidades.Vuelo;
+
 /**
  * @author Bruno
  *
  */
 public interface VueloDAO extends JpaRepository<Vuelo, Long> {
-
+	
 }
