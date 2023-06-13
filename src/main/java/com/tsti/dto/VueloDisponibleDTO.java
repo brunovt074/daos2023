f99ec75966fe3dce19e9debd07a0736aa29a98a2
brunovt074 <brunovt.074@gmail.com>
2023-06-13 00:18:17 -0300

    agregada la clase VueloDisponibleDTO (commit anterior fallo parcialmente)

---
+++ src/main/java/com/tsti/dto/VueloDisponibleDTO.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package com.tsti.dto;
+
+import java.time.LocalDate;
+
+import com.tsti.entidades.Ciudad;
+import com.tsti.entidades.Vuelo;
+import com.tsti.entidades.Vuelo.TipoVuelo;
+
+/**
+ * @author Bruno
+ *
+ */
+public class VueloDisponibleDTO {
+	private Long nroVuelo;
+	private String aerolinea;
+	private Ciudad destino;
+	private TipoVuelo tipoVuelo;
+	private LocalDate fechaPartida;
+	private LocalDate horaPartida;	
+	
+	
+	public VueloDisponibleDTO (Vuelo pojo){
+		
+		super();
+		this.nroVuelo = pojo.getNroVuelo();
+		this.aerolinea = pojo.getAerolinea();
+		this.destino = pojo.getDestino();
+		this.tipoVuelo = pojo.getTipoVuelo();
+		this.fechaPartida = pojo.getFechaPartida();
+		this.horaPartida = pojo.getFechaPartida();
+		
+	}
+
+	public Long getNroVuelo() {
+		return nroVuelo;
+	}
+	
+	public String getAerolinea() {
+		return aerolinea;
+	}
+
+	public void setAerolinea(String aerolinea) {
+		this.aerolinea = aerolinea;
+	}
+
+	public void setNroVuelo(Long nroVuelo) {
+		this.nroVuelo = nroVuelo;
+	}
+
+	public LocalDate getFechaPartida() {
+		return fechaPartida;
+	}
+
+	public void setFechaPartida(LocalDate fechaPartida) {
+		this.fechaPartida = fechaPartida;
+	}
+
+	public LocalDate getHoraPartida() {
+		return horaPartida;
+	}
+
+	public void setHoraPartida(LocalDate horaPartida) {
+		this.horaPartida = horaPartida;
+	}
+
+	public Ciudad getDestino() {
+		return destino;
+	}
+
+	public void setDestino(Ciudad destino) {
+		this.destino = destino;
+	}
+
+	public TipoVuelo getTipoVuelo() {
+		return tipoVuelo;
+	}
+
+	public void setTipoVuelo(TipoVuelo tipoVuelo) {
+		this.tipoVuelo = tipoVuelo;
+	}
+
+	@Override
+	public String toString() {
+		return "VueloDisponibleDTO [nroVuelo=" + nroVuelo + ", fechaPartida=" + fechaPartida + ", horaPartida="
+				+ horaPartida + ", destino=" + destino + "]";
+	}
+	
+	
+}
