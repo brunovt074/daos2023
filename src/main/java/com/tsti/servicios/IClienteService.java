6168bc528f0c89c371e1fbde1f2d157d9640fcd0
Cecilia <lcecilia8147@gmail.com>
2023-06-30 14:39:19 -0300

    Nuevos metodos para Iclienteservice

--- src/main/java/com/tsti/servicios/IClienteService.java
+++ src/main/java/com/tsti/servicios/IClienteService.java
@@ -10,9 +10,10 @@ import com.tsti.entidades.Clientes;
  */
 public interface IClienteService {
 	
+
 	public List<Clientes> getAll();
 	
-	public Optional<Clientes> getById(Long Id);
+	public Optional<Clientes> getById(Long id);
 	
 	public void update(Clientes c);
 	
@@ -20,7 +21,9 @@ public interface IClienteService {
 	
 	public void delete(Long id);
 	
+	public void deleteByDni(Long dni);
+	
 	public List<Clientes> filtrar(String apellido, String nombre);
 	
-	
+	public Optional<Clientes> filtrarPorDni(Long dni);
 }
