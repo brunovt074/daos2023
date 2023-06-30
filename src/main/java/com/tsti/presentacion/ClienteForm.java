a84db2bbfbfc6e1c092363e78762c6ae19c222e5
Cecilia <lcecilia8147@gmail.com>
2023-06-30 14:38:05 -0300

    se agragn datos a cliente form

--- src/main/java/com/tsti/presentacion/ClienteForm.java
+++ src/main/java/com/tsti/presentacion/ClienteForm.java
@@ -82,9 +82,9 @@ public class ClienteForm {
 		c.setDni(this.getDni());
 		c.setApellido(this.getApellido());
 		c.setNombre(this.getNombre());
+		c.setEmail(this.getEmail());
+		c.setFechaNacimiento(this.getFechaNacimiento());
 		c.setNroPasaporte(this.getNroPasaporte());
-		
-		
 		return c;
 	}
 }
